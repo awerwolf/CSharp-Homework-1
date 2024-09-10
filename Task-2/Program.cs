d9217fb8065cdd469d1a5e6e1ec8bdb73be3dff6
awerwolf <awerwol1000@mail.ru>
2024-09-10 17:51:36 +0300

    добавил задание 2

--- Task-2/Program.cs
+++ Task-2/Program.cs
@@ -4,8 +4,44 @@ namespace Task_2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Отправная точка программы
+        /// </summary>
+        static void Main()
         {
+            ReadInteger("X = ", out int x);
+            ReadInteger("Y = ", out int y);
+
+            float k = Expression(x, y);
+            Console.WriteLine(string.Format("X = {0} Y = {0} К = {0:f6}", x, y, k));
         }
+
+        /// <summary>
+        /// Читает введённую строку с консоли и
+        /// пытается спарсить её в целое число
+        /// </summary>
+        /// <param name="prompt">Приглашение к вводу</param>
+        /// <param name="result">Результат парсинга</param>
+        static void ReadInteger(string prompt, out int result)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                if (int.TryParse(Console.ReadLine(), out result))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Введите целое число");
+                }
+            }
+        }
+
+        /// <summary>
+        /// f(x, y) = (138 * x + y) / (x - 24) - (x* y - 67) / (78 + x)
+        /// </summary>
+        static float Expression(float x, float y) =>
+            (138 * x + y) / (x - 24) - (x * y - 67) / (78 + x);
     }
 }
