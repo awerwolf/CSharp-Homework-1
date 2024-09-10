1c0abb301c8d10513ac5b9a65979e118200454af
awerwolf <awerwol1000@mail.ru>
2024-09-10 16:42:59 +0300

    добавил задание 1

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_1
 {
@@ -10,6 +6,18 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Задание №1. Первая программа.");
+
+            // значение префиксного выражения:
+            int r;
+            r = 6;
+            Console.WriteLine(" значение префиксного выражения:");
+            Console.WriteLine(" ++r = " + (++r));
+
+            // значение постфиксного выражения:
+            int t = 6;
+            Console.WriteLine(" значение постфиксного выражения:");
+            Console.WriteLine("до ++ t = " + (t++) + " после ++ t=" + t);
         }
     }
 }
